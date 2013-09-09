1691152fae188efc2e77226038f89ff95bd23a7e
Tapiwa Kelvin <tapiwa@munzwa.tk>
2013-09-10 01:43:39 +0200

    Adds initial project file & dependencies
    Adds django-tastypie-mongoengine as a library

---
+++ forumAPI/urls.py
@@ -0,0 +1,24 @@
+from forumAPI.api import TopicResource
+
+topic_resource = TopicResource()
+
+
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'forumAPI.views.home', name='home'),
+    # url(r'^forumAPI/', include('forumAPI.foo.urls')),
+    #  url(r'^api/v0', include('forumAPI.api.urls')),
+    (r'^api/', include(topic_resource.urls)),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
