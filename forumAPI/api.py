074688916d6a2f5bbea40d367caefa0ef6d23fc1
Kelvin Munzwa <tkmun.zwa@gmail.com>
2013-09-11 00:42:45 +0200

    Added new document & resource types
    - split user document to AdminUser & User resource classes
    - added Comment resource

--- forumAPI/api.py
+++ forumAPI/api.py
@@ -5,5 +5,26 @@ from forumAPI import documents
 class TopicResource(resources.MongoEngineResource):
     class Meta:
         queryset = documents.Topic.objects.all()
-        allowed_methods = ('get', 'post', 'put', 'delete')
+        allowed_methods = ('get', 'post', 'put', 'patch', 'delete') #TODO: own submission auth
         authorization = authorization.Authorization()
+        
+class CommentResource(resources.MongoEngineResource):
+    class Meta:
+        queryset = documents.Comment.objects.all()
+        allowed_methods = ('get', 'post', 'put', 'patch', 'delete')
+        authorization = authorization.Authorization() #TODO: own-submission authorization, allow admin to put/patch
+
+class UserResource(resources.MongoEngineResource):
+    class Meta:
+        queryset = documents.User.objects.filter(is_admin__ne=True)
+        allowed_methods = ('get', 'post', 'put', 'patch', 'delete')
+        authorization = authorization.Authorization() #TODO: own-submission authorization for get, put, patch, delete any for post
+        excludes = ['is_admin', 'password']
+
+class AdminUserResource(resources.MongoEngineResource):
+    class Meta:
+        queryset = documents.User.objects.filter(is_admin=True)
+        allowed_methods = ('get', 'post', 'put', 'patch', 'delete')
+        authorization = authorization.Authorization() #TODO: admin-only authorization
+        excludes = ['is_admin', 'password']
+
--- forumAPI/documents.py
+++ forumAPI/documents.py
@@ -12,7 +12,23 @@ class InheritableEmbeddedDocument(mongoengine.EmbeddedDocument):
         'abstract': True,
         'allow_inheritance': True,
     }
+    
+class User(mongoengine.Document):
+    email = mongoengine.EmailField(required=True, unique=True)
+    is_admin = mongoengine.BooleanField()
+    first_name = mongoengine.StringField(max_length=50)
+    last_name = mongoengine.StringField(max_length=50)
+
 class Topic(InheritableDocument):
+    author = mongoengine.ReferenceField(User, dbref=False)
     name = mongoengine.StringField(max_length=200, required=True)
     blurb = mongoengine.StringField(required=False)
+
+class Comment(InheritableDocument):
+    topic = mongoengine.ReferenceField(Topic, required=True, reverse_delete_rule=mongoengine.CASCADE, dbref=False)
+    author = mongoengine.ReferenceField(User, reverse_delete_rule=mongoengine.NULLIFY, dbref=False)
+    in_reply_to = mongoengine.StringField(max_length=50)#mongoengine.ReferenceField(Comment)
+    comment_body = mongoengine.StringField(required=True)
+    is_hidden = mongoengine.BooleanField()
+    
     
