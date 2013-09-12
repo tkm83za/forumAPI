9719a90a0c54e7ce89c06ce20973588ba22eeeee
Kelvin Munzwa <tkmun.zwa@gmail.com>
2013-09-12 02:15:01 +0200

    Updates URLs to support static resources and client URLs

--- forumAPI/urls.py
+++ forumAPI/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 from tastypie.api import Api
+
+from forumSite import urls as site_urls
 from forumAPI.api import TopicResource, CommentResource, UserResource, AdminUserResource
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 v1_api = Api(api_name='v1')
 v1_api.register(TopicResource())
@@ -11,6 +18,10 @@ v1_api.register(AdminUserResource())
 
 
 urlpatterns = patterns('',
-    # The normal jazz here...
     (r'^api/', include(v1_api.urls)),
+    ('^admin/', include(admin.site.urls)),
+    (r'^static/', 'django.views.static.serve', {'document_root' : 'forumAPI/static', 'show_indexes' : True}),
+    ('', include(site_urls)),
 )
+
+urlpatterns = staticfiles_urlpatterns() + urlpatterns
